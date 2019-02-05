8103a1cb7fa0b853dacd1b3a83c4ef62d27e7829
Matt Keller <von.helmut@gmail.com>
2019-02-05 09:12:52 -0500

    cleanup, rearrange

--- docs/js/howard.js
+++ docs/js/howard.js
@@ -1,3 +1,105 @@
+var sound = false;
+window.onload = howNow();
+const updateBubble = setInterval(howNow, 90000);
+
+function howNow() {
+	applyTheme(19, 5);
+
+	if (checkDay(4, 4)) {
+		document.getElementById("phrase").innerHTML =
+			"<a href=\"http://www.upc-online.org/respect/\">\
+			Happy International Respect for Chickens Day.</a>";
+	} else {
+		getQuotes();
+	}
+
+	fadeIn(document.getElementById("bub"));
+
+	if (sound === true) {
+		sayIt = setTimeout(sayQuote, 1000); // needs a delay
+	}
+}
+
+function applyTheme(startNightHour, endNightHour) {
+	const hour = new Date().getHours();
+
+	if (hour > startNightHour || hour < endNightHour) {
+		document.body.className = "night";
+	} else {
+		document.body.className = "day";
+	}
+}
+
+// month is 0..11, day is 1..31
+function checkDay(month, day) {
+	const d = new Date();
+
+	if (d.getMonth() === month && d.getDate() === day) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// fade in - https://gist.github.com/chrisbuttery/cf34533cbb30c95ff155
+function fadeIn(el, display) {
+	el.style.opacity = 0;
+	el.style.display = display || "block";
+
+	(function fade() {
+		var val = parseFloat(el.style.opacity) + 0.1;
+		if (val < 1) {
+			el.style.opacity = val;
+			requestAnimationFrame(fade);
+		}
+	}());
+}
+
+function toggleSound() {
+	if (sound === false) {
+		sound = true;
+		sayQuote();
+	} else {
+		sound = false;
+	}
+}
+
+function sayQuote() {
+	if (! 'speechSynthesis' in window) {
+		return;
+	}
+
+	var text = document.getElementById("phrase").innerHTML;
+	// remove tags or they're spoken
+	text = text.replace(/(<a href[^>]*>|<\/a>)/g, "");
+	var msg = new SpeechSynthesisUtterance();
+	var voices = window.speechSynthesis.getVoices();
+
+	// set default/fallback voice
+	msg.voice = voices[0];
+
+	// we prefer a British accent (Daniel, hopefully)
+	for (i = 0; i < voices.length; i++) {
+		if (voices[i].lang == 'en-GB') {
+			if (voices[i].name == 'Daniel') {
+				msg.voice = voices.filter(function(voice) {
+					return voice.name == 'Daniel'; })[0];
+			} else {
+				msg.voice = voices.filter(function(voice) {
+					return voice.lang == 'en-GB'; })[0];
+			}
+			break;
+		}
+	}
+
+	msg.volume = 1;
+	msg.rate = 1;
+	msg.pitch = 1;
+	msg.text = text;
+
+	speechSynthesis.speak(msg);
+}
+
 function getQuotes() {
 	if (sessionStorage.length < 1) {
 		var xhr = new XMLHttpRequest();
@@ -44,104 +146,3 @@ function pickQuote(quotes) {
 		document.getElementById("phrase").innerHTML = quotes.data[pick].text;
 	}
 }
-
-function applyTheme(startNightHour, endNightHour) {
-	const hour = new Date().getHours();
-
-	if (hour > startNightHour || hour < endNightHour) {
-		document.body.className = "night";
-	} else {
-		document.body.className = "day";
-	}
-}
-
-// month is 0..11, day is 1..31
-function checkDay(month, day) {
-	const d = new Date();
-
-	if (d.getMonth() === month && d.getDate() === day) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-// fade in - https://gist.github.com/chrisbuttery/cf34533cbb30c95ff155
-function fadeIn(el, display) {
-	el.style.opacity = 0;
-	el.style.display = display || "block";
-
-	(function fade() {
-		var val = parseFloat(el.style.opacity) + 0.1; // 'let'
-		if (val < 1) {
-			el.style.opacity = val;
-			requestAnimationFrame(fade);
-		}
-	}());
-}
-
-var sound = 0;
-
-function toggleSound() {
-	if (sound === 0) {
-		sound = 1;
-	} else {
-		sound = 0;
-	}
-
-	if (sound === 1) {
-		sayQuote();
-	}
-}
-
-function sayQuote() {
-	if (! 'speechSynthesis' in window) {
-		return;
-	}
-
-	var text = document.getElementById("phrase").innerHTML;
-	text = text.replace(/(<a href[^>]*>|<\/a>)/g, ""); // remove tags or they're spoken
-	var msg = new SpeechSynthesisUtterance();
-	var voices = window.speechSynthesis.getVoices();
-
-	// set default (fallback) voice
-	msg.voice = voices[0];
-
-	// we prefer a British accent (Daniel, hopefully)
-	for (i = 0; i < voices.length; i++) {
-		if (voices[i].lang == 'en-GB') {
-			if (voices[i].name == 'Daniel') {
-				msg.voice = voices.filter(function(voice) { return voice.name == 'Daniel'; })[0];
-			} else {
-				msg.voice = voices.filter(function(voice) { return voice.lang == 'en-GB'; })[0];
-			}
-			break;
-		}
-	}
-
-	msg.volume = 1;
-	msg.rate = 1;
-	msg.pitch = 1;
-	msg.text = text;
-
-	speechSynthesis.speak(msg);
-}
-
-function howNow() {
-	applyTheme(19, 5);
-
-	if (checkDay(4, 4)) {
-		document.getElementById("phrase").innerHTML =
-			"<a href=\"http://www.upc-online.org/respect/\">\
-			Happy International Respect for Chickens Day.</a>";
-	} else {
-		getQuotes();
-	}
-	if (sound === 1) {
-		sayIt = setTimeout(sayQuote, 1000); // needs a delay
-	}
-	fadeIn(document.getElementById("bub"));
-}
-
-window.onload = howNow();
-const updateBubble = setInterval(howNow, 90000);
